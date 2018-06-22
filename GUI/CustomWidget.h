bd6d741e72e467a81ba4b6ef1b44346a6537c133
Kamil Lewan <carmel4a97@gmail.com>
2018-06-22 13:47:12 +0200

    Refactor `CustomWidget`. - formating - some comments - delete unsued stuff

--- GUI/CustomWidget.h
+++ GUI/CustomWidget.h
@@ -11,55 +11,83 @@
 #ifndef _CUSTOM_WIDGET_
 #define _CUSTOM_WIDGET_
 
-#include <string>
-#include <memory.h>
+#include <string> // std::string
+#include <memory.h> // std::shared_pt
+#include <unordered_map> // std::unordered_map
+
 #include "GUI.h"
 #include "nanogui/nanogui.h"
 
 using namespace nanogui;
-class GUI_;
-class CustomWidget;
+
+class GUI_; class CustomWidget;
 
 typedef std::shared_ptr< CustomWidget > shared_custom_widget;
 typedef std::unordered_map< std::string, shared_custom_widget > widget_map;
 
-/*! A Base class to create own widgets. It is meant as container for nanogui::ref to 
- * widget. It doesn't implement own logic, as creating widget, so in overrided
- * constructor, and virtual methods user must include manually assign nanogui::Widget
- * to nanogui::ref.
- */
+/// A Base class to create own widgets.
+/**
+  It is meant as container for nanogui::ref to 
+  widget. It doesn't implement own logic, as creating widget, so in overrided
+  constructor, and virtual methods user must include manually assign nanogui::Widget
+  to nanogui::ref.
+*/
 class CustomWidget{
     
-    public:
-        CustomWidget(){};
-        virtual ~CustomWidget(){};
-        Widget* widget(){ return widget_.get(); };
-        nanogui::ref<Widget> widget_ref(){ return widget_; };
-        // Called before adding to `GUI::widget` map.
-        virtual void init() = 0;
-        virtual void make() = 0;
+  public:
+    CustomWidget(){};
+    virtual ~CustomWidget(){};
 
-        virtual void show(){ may_update = true; }
-        virtual void hide(){ may_update = false; }
-        // Except init, you don't want to call this directly.
-        virtual void resize( Vector2i v ){};
-        virtual void update(){};
+    /////////////
+    // Getters //
+    /////////////
 
-        const widget_map* owner;
-        bool may_update = false;
-        // To print name of widget.
-        operator std::string(){
-            for( auto const& x : *owner ) {
-                if( x.second.get() == this ) return( x.first );
-            }
-            std::string s = "No *CustomWidget fund in owner!";
-            throw s;
-        };
+    /** Return raw pointer to main Widget */
+    Widget* widget(){ return widget_.get(); };
+    /** Return nanogui reference to Widget */
+    nanogui::ref<Widget> widget_ref(){ return widget_; };
+
+    //////////////////////////
+    // Pure virtual methods //
+    //////////////////////////
+
+    /** Called before adding to `GUI::widget` map. */
+    virtual void init() = 0;
+    /** Called after adding to `GUI::widget` map. */
+    virtual void make() = 0;
+
+    /////////////////////
+    // Virtual methods //
+    /////////////////////
+
+    virtual void show(){ may_update = true; }
+    virtual void hide(){ may_update = false; }
+
+    /** Resize widget, due to onw behaviour.
+     *  Except init, you don't want to call this directly.
+     *  Set anchor (position, size - layout) here.
+     */
+    virtual void resize( Vector2i v ){};
+
+    /** Update content of widget, due to onw behaviour.
+     *  Currently it's done every frame for every widget,
+     *  so don't call that directly.
+     */
+    virtual void update(){};
+    bool may_update = false;
+
+    const widget_map* owner;
+    /** To print name of widget. */
+    operator std::string(){
+        for( auto const& x : *owner ) {
+            if( x.second.get() == this ) return( x.first );
+        }
+        std::string s = "No *CustomWidget fund in owner!";
+        throw s;
+    };
         
-    protected:
-        GUI_::Anchor x_anchor;
-        GUI_::Anchor y_anchor;
-        nanogui::ref<Widget> widget_;
+  protected:
+    nanogui::ref<Widget> widget_; /**< Main managed widget. */
 };
 
 #endif // !_CUSTOM_WIDGET_
